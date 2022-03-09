3ffab8028bafb1c04efeca003b6247a54d2d6132
Wesley Duerksen <wesleypacs@gmail.com>
2022-03-09 15:32:21 -0800

    Swapping behavior complete (44 minutes)

--- src/Components/Piece.svelte
+++ src/Components/Piece.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
-    export let x: number, y: number, pieceClass: string;
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    export let x: number, y: number, pieceClass: string, ID: number;
+
+    function clickEvent() {
+        dispatch("clickEvent", ID);
+    }
 </script>
 
 <div
     style="background-position: {-150 * x}px {-150 * y}px;"
     class={pieceClass}
+    on:click={clickEvent}
 />
 
 <style>
